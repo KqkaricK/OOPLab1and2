59d23448c815117effbb6b62bf9a9e58a2ef7260
KqkaricK <andrew73-42@yandex.ru>
2021-04-24 22:05:25 +0700

    Добавьте файлы проекта.

---
+++ WindowsFormsApplication9/Class_Izd.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication9
+{
+    class Class_Izd
+    {
+        private string name;
+        private int id;
+        private int god;
+        public Class_Izd(string name, int id, int god)
+        {
+            this.name = name;
+            this.id = id;
+            this.god = god;
+        }
+        public Class_Izd()
+        {
+            name = "Ящик";
+            id = 0;
+            god = 1991;
+        }
+        public string GetName()
+        {
+            return name;
+        }
+        public void SetName(string name)
+        {
+            this.name = name;
+        }
+        public int GetId()
+        {
+            return id;
+        }
+        public void SetId(int id)
+        {
+            this.id = id;
+        }
+        public int GetGod()
+        {
+            return god;
+        }
+        public void SetGod(int god)
+        {
+            this.god = god;
+        }
+    }
+    class Strana : Class_Izd
+    {
+        string strana;
+        public Strana(string strana, string name, int id, int god) 
+        : base(name, id, god)
+        {
+            this.strana = strana;
+        }
+        public Strana()
+        {
+            strana = "Unknow";
+        }
+        public string GetStrana()
+        {
+            return strana;
+        }
+        public void SetStrana(string strana)
+        {
+            this.strana = strana;
+        }
+    }
+    class Eks : Class_Izd
+    {
+        int kol;
+        public Eks(int kol, string name, int id, int god)
+        : base(name, id, god)
+        {
+            this.kol = kol;
+        }
+        public Eks()
+        {
+            kol = 0;
+        }
+        public int GetKol()
+        {
+            return kol;
+        }
+        public void SetKol(int kol)
+        {
+            this.kol = kol;
+        }
+    }
+    class Tema : Class_Izd
+    {
+        string tema;
+        public Tema(string tema, string name, int id, int god)
+        : base(name, id, god)
+        {
+            this.tema = tema;
+        }
+        public Tema()
+        {
+            tema = "Unknow";
+        }
+        public string GetTema()
+        {
+            return tema;
+        }
+        public void SetTema(string tema)
+        {
+            this.tema = tema;
+        }
+    }
+}
---
+++ WindowsFormsApplication9/Form1.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked == true)
+            {
+                Strana a = new Strana();
+                if (textBox1.Text == "" || textBox2.Text == "" || textBox3.Text == "" || textBox4.Text == "")
+                {
+                    richTextBox1.Text += "Имя: ";
+                    richTextBox1.Text += a.GetName();
+                    richTextBox1.Text += Environment.NewLine;
+                    richTextBox1.Text += "Id: ";
+                    richTextBox1.Text += a.GetId();
+                    richTextBox1.Text += Environment.NewLine;
+                    richTextBox1.Text += "Год: ";
+                    richTextBox1.Text += a.GetGod();
+                    richTextBox1.Text += Environment.NewLine;
+                    richTextBox1.Text += "Страна: ";
+                    richTextBox1.Text += a.GetStrana();
+                    richTextBox1.Text += Environment.NewLine;
+                }
+                else
+                {
+                    a.SetName(textBox1.Text);
+                    a.SetId(Convert.ToInt32(textBox2.Text));
+                    a.SetGod(Convert.ToInt32(textBox3.Text));
+                    a.SetStrana(textBox4.Text);
+                    richTextBox1.Text += "Имя: ";
+                    richTextBox1.Text += a.GetName();
+                    richTextBox1.Text += Environment.NewLine;
+                    richTextBox1.Text += "Id: ";
+                    richTextBox1.Text += a.GetId();
+                    richTextBox1.Text += Environment.NewLine;
+                    richTextBox1.Text += "Год: ";
+                    richTextBox1.Text += a.GetGod();
+                    richTextBox1.Text += Environment.NewLine;
+                    richTextBox1.Text += "Страна: ";
+                    richTextBox1.Text += a.GetStrana();
+                    richTextBox1.Text += Environment.NewLine;
+                }
+            }
+            else if (radioButton2.Checked == true)
+            {
+                Class_Izd a = new Class_Izd();
+                if (textBox1.Text == "" || textBox2.Text == "" || textBox3.Text == "")
+                {
+                    richTextBox1.Text += "Имя: ";
+                    richTextBox1.Text += a.GetName();
+                    richTextBox1.Text += Environment.NewLine;
+                    richTextBox1.Text += "Id: ";
+                    richTextBox1.Text += a.GetId();
+                    richTextBox1.Text += Environment.NewLine;
+                    richTextBox1.Text += "Год: ";
+                    richTextBox1.Text += a.GetGod();
+                    richTextBox1.Text += Environment.NewLine;
+                }
+                else
+                {
+                    a.SetName(textBox1.Text);
+                    a.SetId(Convert.ToInt32(textBox2.Text));
+                    a.SetGod(Convert.ToInt32(textBox3.Text));
+                    richTextBox1.Text += "Имя: ";
+                    richTextBox1.Text += a.GetName();
+                    richTextBox1.Text += Environment.NewLine;
+                    richTextBox1.Text += "Id: ";
+                    richTextBox1.Text += a.GetId();
+                    richTextBox1.Text += Environment.NewLine;
+                    richTextBox1.Text += "Год: ";
+                    richTextBox1.Text += a.GetGod();
+                    richTextBox1.Text += Environment.NewLine;
+                }
+            }
+            else if (radioButton3.Checked == true)
+            {
+                Eks a = new Eks();
+                if (textBox1.Text == "" || textBox2.Text == "" || textBox3.Text == "" || textBox4.Text == "")
+                {
+                    richTextBox1.Text += "Имя: ";
+                    richTextBox1.Text += a.GetName();
+                    richTextBox1.Text += Environment.NewLine;
+                    richTextBox1.Text += "Id: ";
+                    richTextBox1.Text += a.GetId();
+                    richTextBox1.Text += Environment.NewLine;
+                    richTextBox1.Text += "Год: ";
+                    richTextBox1.Text += a.GetGod();
+                    richTextBox1.Text += Environment.NewLine;
+                    richTextBox1.Text += "Кол.: ";
+                    richTextBox1.Text += a.GetKol();
+                    richTextBox1.Text += Environment.NewLine;
+                }
+                else
+                {
+                    if (ChekIsNum(textBox4.Text, label4.Text) == true)
+                    {
+                        a.SetName(textBox1.Text);
+                        a.SetId(Convert.ToInt32(textBox2.Text));
+                        a.SetGod(Convert.ToInt32(textBox3.Text));
+                        a.SetKol(Convert.ToInt32(textBox4.Text));
+                        richTextBox1.Text += "Имя: ";
+                        richTextBox1.Text += a.GetName();
+                        richTextBox1.Text += Environment.NewLine;
+                        richTextBox1.Text += "Id: ";
+                        richTextBox1.Text += a.GetId();
+                        richTextBox1.Text += Environment.NewLine;
+                        richTextBox1.Text += "Год: ";
+                        richTextBox1.Text += a.GetGod();
+                        richTextBox1.Text += Environment.NewLine;
+                        richTextBox1.Text += "Кол.: ";
+                        richTextBox1.Text += a.GetKol();
+                        richTextBox1.Text += Environment.NewLine;
+                    }
+                }
+            }
+            else if (radioButton4.Checked == true)
+            {
+                Tema a = new Tema();
+                if (textBox1.Text == "" || textBox2.Text == "" || textBox3.Text == "" || textBox4.Text == "")
+                {
+                    richTextBox1.Text += "Имя: ";
+                    richTextBox1.Text += a.GetName();
+                    richTextBox1.Text += Environment.NewLine;
+                    richTextBox1.Text += "Id: ";
+                    richTextBox1.Text += a.GetId();
+                    richTextBox1.Text += Environment.NewLine;
+                    richTextBox1.Text += "Год: ";
+                    richTextBox1.Text += a.GetGod();
+                    richTextBox1.Text += Environment.NewLine;
+                    richTextBox1.Text += "Тема: ";
+                    richTextBox1.Text += a.GetTema();
+                    richTextBox1.Text += Environment.NewLine;
+                }
+                else
+                {
+                    a.SetName(textBox1.Text);
+                    a.SetId(Convert.ToInt32(textBox2.Text));
+                    a.SetGod(Convert.ToInt32(textBox3.Text));
+                    a.SetTema(textBox4.Text);
+                    richTextBox1.Text += "Имя: ";
+                    richTextBox1.Text += a.GetName();
+                    richTextBox1.Text += Environment.NewLine;
+                    richTextBox1.Text += "Id: ";
+                    richTextBox1.Text += a.GetId();
+                    richTextBox1.Text += Environment.NewLine;
+                    richTextBox1.Text += "Год: ";
+                    richTextBox1.Text += a.GetGod();
+                    richTextBox1.Text += Environment.NewLine;
+                    richTextBox1.Text += "Тема: ";
+                    richTextBox1.Text += a.GetTema();
+                    richTextBox1.Text += Environment.NewLine;
+                }
+            }
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            label4.Text = "Страна";
+            ChekRadioButton(sender);
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            label4.Text = "Кол. Экземпляров";
+            ChekRadioButton(sender);
+        }
+
+        bool ChekIsNum(string sender, string label)
+        {
+            int num = 0;
+            if (Int32.TryParse(sender, out num))
+            {
+                return true;
+            }
+            else
+            {
+                MessageBox.Show("В " + label + " должны быть только цифры!");
+                return false;
+            }
+        }
+
+        void ChekRadioButton(object sender)
+        {
+            RadioButton checkBox = (RadioButton)sender;
+            if (checkBox.Checked == true)
+            {
+                label4.Visible = true;
+                textBox4.Visible = true;
+            }
+            else
+            {
+                label4.Visible = false;
+                textBox4.Visible = false;
+            }
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+            label4.Text = "Тема";
+            ChekRadioButton(sender);
+        }
+    }
+}
